358c908f0f97ba8fc60f633902fcca96d37cf450
Mendez <yerkowilliams@gmail.com>
2017-09-16 14:58:43 -0300

    commit de prueba

---
+++ src/main/java/com/achha/symachha/SymAchhaApplication.java
@@ -0,0 +1,12 @@
+package com.achha.symachha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymAchhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymAchhaApplication.class, args);
+	}
+}
